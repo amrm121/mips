7bdf30b8fbf39e239993ab836845be8d20545cba
Alexandre Moreno Ribeiro Magalhães <amrm@cin.ufpe.br>
2018-03-20 14:58:08 -0300

    exercicio finalizado

--- 3.s
+++ 3.s
@@ -1,34 +1,28 @@
-#(lea) Faça um programa em linguagem de montagem MIPS que salve numa variável na 
-#memória chamada NA a quantidade de ocorrências do caractere caractere ‘a’ em uma string, 
-#de tamanho pré-determinado, que estará armazenada na memória.
-
-#DICA: 1 caractere ocupa exatamente 1byte(ou 8bits) na memória
-#	coloque em um registrador o endereço da memória a ser lido
 .data
 NA: .word 0
-str: .asciiz "Palavra a ser contada os 'a's"
-char: .byte a
+str: .asciiz "String a ser contado os 'a's"
+char: .byte 'a'
 
+.globl main
 .text
-.globl start
-.ent start
-
-start:
-li $8, 0#indice do contador
-la $9, str #t1 = str(adr) 
-lb $10, char
-lw $2, NA #$v0 = NA
+#regs 5-7 os valores nao sao perdidos entre chamadas
+main:
+li $2, 10 #syscall SPIM 10 = EXIT
+la $6, str #t1 = str(adr) 
+lb $7, char
+lw $5, NA
 
 count:
-lb $4, 0($9)
+lb $4, 0($6)
 beq $4, $0, fim
-addi $9, $9, 1 #adiciono +1 byte no end p acessar prox char
-beq $4, $10, countchar
+addi $6, $6, 1 #adiciono +1 byte no end p acessar prox char
+beq $4, $7, countchar #se str[i] = 'a' goto countchar
+j count
+
 
 countchar:
-addi $8, $8, 1
+addi $5, $5, 1 #contador NA
 j count
 
 fim:
-break;
-.end start
+syscall
