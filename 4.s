e37066c30d2c9b7bfc1c15d080d5be455a7e46ac
Alexandre Moreno Ribeiro Magalhães <amrm@cin.ufpe.br>
2018-03-22 17:49:41 -0300

    qtSpim

--- 4.s
+++ 4.s
@@ -1,28 +1,33 @@
 .data
 ctd	: .word 0
-str: .asciiz "1,5,0,7,2"
+str: .asciiz "1,5,7,2"
 zb: .byte '0'
+vb: .byte ','
+ans: .word 1
 .text
 
-.globl start
-.ent start
-start:  
+.globl main
+.ent main
+main:  
+li $2, 10
 la $16, str #$s0 = str(adr) 
-lb $17, zb #$s1 = '0' = 30 hx ascii
-lw $20, ctd #var contador reg $s4
+lb $17, zb #$s1 = '0' = 30 hx ascii 
+lb $18, vb #hx da virgula
+lw $19, ans #res multp reg $s3
+lw $20, ctd #var contador de nums reg $s4
+
 
 count:
 lb $4, 0($16)
 beq $4, $0, fim #se = 0 fim da string
 addi $16, $16, 1 #adiciono +1 byte no end p acessar prox char
-beq $4, $17, virgula #verificar virgula, 2c em hx
-subu $5, $4, $17 #$5 fica com o 'int' do resultado do hx do byte carregado em $4 menos o hx do reg zb '0' = 30
-#beq $4, $17, countchar se str[i] = 'a' goto countchar
+beq $4, $18, count #verificar virgula, 2c em hx
+sub $5, $4, $17 #reg $5 fica com o 'int' do resultado do hx do byte carregado em $4 menos o hx do reg zb '0' = 30
+addi $20, 1 #conto +1 digito
+mul $19, $19, $5  
 j count
 
 
-virgula:
-
 fim:
-break
-.end start
+syscall
+.end main
