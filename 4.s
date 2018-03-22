2b47996b7b5d15ffff97d69557227eaf61b2ef73
Alexandre Moreno Ribeiro Magalhães <amrm@cin.ufpe.br>
2018-03-22 12:08:53 -0300

    4 mipsit
    falta verificar as virgulas, hexa resolvido

--- 4.s
+++ 4.s
@@ -1,21 +1,28 @@
 .data
-str: .asciiz "1,5,2,3" #string a ser lida
-ct: .word 0 #contador
-
-.globl main
+ctd	: .word 0
+str: .asciiz "1,5,0,7,2"
+zb: .byte '0'
 .text
 
-main:
-li $2, 10 #v0 = $2
-lw $4, ct #contador reg 4 .. a0-a3 $4-$7
-la $5, str #addr(str) reg 5
+.globl start
+.ent start
+start:  
+la $16, str #$s0 = str(adr) 
+lb $17, zb #$s1 = '0' = 30 hx ascii
+lw $20, ctd #var contador reg $s4
 
-loop:
-lb $6, 0($5)
-#sub por 30 = int em hexadecimal
-beq $6, $0, fim
-addi $5, $5, 2
-j loop
+count:
+lb $4, 0($16)
+beq $4, $0, fim #se = 0 fim da string
+addi $16, $16, 1 #adiciono +1 byte no end p acessar prox char
+beq $4, $17, virgula #verificar virgula, 2c em hx
+subu $5, $4, $17 #$5 fica com o 'int' do resultado do hx do byte carregado em $4 menos o hx do reg zb '0' = 30
+#beq $4, $17, countchar se str[i] = 'a' goto countchar
+j count
+
+
+virgula:
 
 fim:
-syscall
+break
+.end start
