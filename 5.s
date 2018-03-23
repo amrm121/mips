cc256f6de8719faa516fd9c985d7f3806aee136a
Alexandre Moreno Ribeiro Magalhães <amrm@cin.ufpe.br>
2018-03-23 16:03:08 -0300

    q6

--- 5.s
+++ 5.s
@@ -1,3 +1,32 @@
 .data
-str: .asciiz "885688568697" #string a ser decodificada xaxado
-ct: .word 0 #contador
+a: .word 0
+b: .word 20
+c: .word 14
+.text
+
+.globl start
+.ent start
+start:  
+lw $6, a
+lw $7, b
+lw $8, c
+li $16, 10
+loop:
+beq $6, $16, branch
+addi $6, 1
+
+for($6=0 ; $6<10 ; $6++){
+		if($7 < $8){
+			$9 = 15;
+			$10 ++;
+		}
+		else{
+			$9 = 13;
+			$8++;
+}
+	}
+	if($8> $10)
+		$11 = 1;
+}
+
+.end start
