e3c03ad8ac83b83031e038531e39f594c46076bd
Alexandre Moreno Ribeiro Magalhães <amrm@cin.ufpe.br>
2018-03-23 15:57:32 -0300

    sem mult

--- 4.s
+++ 4.s
@@ -1,15 +1,14 @@
 .data
 ctd	: .word 0
-str: .asciiz "1,5,7,2"
+str: .asciiz "2,2,5,2"
 zb: .byte '0'
 vb: .byte ','
 ans: .word 1
 .text
 
-.globl main
-.ent main
-main:  
-li $2, 10
+.globl start
+.ent start
+start:  
 la $16, str #$s0 = str(adr) 
 lb $17, zb #$s1 = '0' = 30 hx ascii 
 lb $18, vb #hx da virgula
@@ -24,10 +23,10 @@ addi $16, $16, 1 #adiciono +1 byte no end p acessar prox char
 beq $4, $18, count #verificar virgula, 2c em hx
 sub $5, $4, $17 #reg $5 fica com o 'int' do resultado do hx do byte carregado em $4 menos o hx do reg zb '0' = 30
 addi $20, 1 #conto +1 digito
-mul $19, $19, $5  
+mul $19, $5, $19 #n consegui resolver a multiplicacao
 j count
 
 
 fim:
-syscall
-.end main
+break
+.end start
